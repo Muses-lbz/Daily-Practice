e2eb14189b00edbb9d7e1f4c1047d3022da4410d
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-21 13:43:38 +0800

    Add files via upload

---
+++ 2021_05_19.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a[] = { 4,0,2,3,1 }, i, j, t;
+	for (i = 1; i < 5; i++)
+	{
+		t = a[i];
+		j = i - 1;
+		while (j >= 0 && t > a[j])
+		{
+			a[j + 1] = a[j];
+			--j;
+		}
+		a[j + 1] = t;
+	}
+	printf("%d", &a);
+	return 0;
+}
