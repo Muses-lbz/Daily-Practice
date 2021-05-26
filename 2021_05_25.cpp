b52b580bd9c19daad72b3ed3798f1a302d7462b4
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-26 09:59:04 +0800

    Add files via upload

---
+++ 2021_05_25.cpp
@@ -0,0 +1,41 @@
+﻿//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	printf("I like the simple pleasures in life.\n我喜欢生活中那些简单的乐趣。\n");
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int k;
+//	k = 7 >> 1;
+//	printf("%d", k);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int n;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		if (n % 3 == 0) continue;
+//		printf("%d", n);
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+	printf("%s,%5.3s\n", "computer", "computer");
+	return 0;
+}
