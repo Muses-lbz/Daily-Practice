519fcf542d499dcc7643bd6ee3c36bae6d641c9d
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-31 08:23:03 +0800

    Update A boring code.cpp

--- A boring code.cpp
+++ A boring code.cpp
@@ -3,8 +3,7 @@
 
 int main()
 {
-	printf("It's always important to forgive oneself.\n");
-	printf("人生很长，要学会与自己和解。\n");
+	printf("We cherish each moment of our lives.\n珍惜生命中的每一刻。\n");
 	printf("\n");
 	system("pause");
 	return 0;
