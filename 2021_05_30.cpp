fd41804acbfde09f0f962e2cf1732b6e5ee2ce2e
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-06-01 11:44:25 +0800

    Add files via upload

---
+++ 2021_05_30.cpp
@@ -0,0 +1,25 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int  i = 1, j = 1, k = 2; 
+//	if ((j++ || k++) && i++) 
+//		printf("%d,%d,%d\n", i, j, k);
+//	system("pause");
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#define M(x,y,z) x*y+z
+
+int main()
+{
+	int a = 1, b = 2, c = 3;
+	printf("%d\n", M(a + b, b + c, c + a));
+	system("pause");
+	return 0;
+}
