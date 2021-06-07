61136940d1661e6e55c88c053c13fb346da2f776
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-06-07 21:34:18 +0800

    Add files via upload

---
+++ 2021_06_06.cpp
@@ -0,0 +1,49 @@
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int i, j, mul;
+//	for (i = 1; i <= 9; ++i)
+//	{
+//		for (j = 1; j <= 9; ++j)
+//		{
+//			mul = i * j;
+//			printf("%d*%d=%2d\t", i, j, mul);
+//			if (j % 9 == 0)
+//				printf("\n");
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+////闰年判断
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int year;
+//	scanf_s("%d", &year);
+//	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+//	{
+//		printf("It is a leap year!\n");
+//	}
+//	else
+//	{
+//		printf("It is not a leap year!\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("hello world\n");
+	system("pause");
+	return 0;
+}
