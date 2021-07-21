9610089015ff107ae33d86d21ba7c99e0ae1fd9b
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-07-22 07:13:51 +0800

    Update 2021_05_29.cpp

--- 2021_05/2021_05_29.cpp
+++ 2021_05/2021_05_29.cpp
@@ -1,4 +1,4 @@
-﻿//#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
 //#include<stdio.h>
 //#include<stdlib.h>
 //
@@ -110,8 +110,7 @@
 //	return 0;
 //}
 
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
 
 void func(int* p)
 {
