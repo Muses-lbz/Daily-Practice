037ca504407fc194549bd2f4542c67e7a5923be1
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-09 10:41:13 +0800

    Add files via upload

---
+++ 2021_08/2021_08_08.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int u = 0;
+	int v = 0;
+
+	printf("Th%cs is %c te%cting program%c!!\n", 'i', 'a', 's', '!');
+	
+	scanf_s("ooo%dooo%dooo", &u, &v);
+	//scanf语句，要严格地按照输入格式输入
+	printf("u = %d, v = %d.\n", u, v);
+
+	return 0;
+}
