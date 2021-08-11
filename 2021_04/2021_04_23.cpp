f188a56a620435f6e5caed581b7e7b13de0af1cd
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-11 17:13:50 +0800

    Create 2021_04_23.cpp

---
+++ 2021_04/2021_04_23.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int X()
+{
+	printf("蔺");
+	return 0;
+}
+
+int Y()
+{
+	printf("兵");
+	return 0;
+}
+
+int Z()
+{
+	printf("姿");
+	return 0;
+}
+
+int main()
+{
+	X();
+	Y();
+	Z();
+	printf("\n");
+	Y();
+	Z();
+	printf("\n");
+	system("pause");
+	return 0;
+}
