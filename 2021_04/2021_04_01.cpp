bbfae0c80e456e0bbee26869d306512287773c52
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-20 18:27:57 +0800

    Update 2021_04_01.cpp

--- 2021_04/2021_04_01.cpp
+++ 2021_04/2021_04_01.cpp
@@ -1,9 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
 
 int main()
 {
-	printf("this is a program!\n");
+	char a[5][11], t[11];
+	int i, j;
+	for (i = 0; i <= 4; i++)
+	{
+		gets_s(a[i]);
+	}
+	for (i = 0; i <= 3; i++)
+	{
+		for (j = i + 1; j <= 4; j++)
+		{
+			if (strcmp(a[i], a[j]) > 0)
+			{
+				strcpy_s(t, a[i]);
+				strcpy_s(a[i], a[j]);
+				strcpy_s(a[j], t);
+			}
+		}
+	}
+	for (i = 0; i <= 4; i++)
+	{
+		puts(a[i]);
+	}
 	system("pause");
 	return 0;
 }
+
