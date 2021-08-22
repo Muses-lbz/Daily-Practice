03cd0ee0fccec5292e825d2e21b2805cfd0a0a7b
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-22 22:53:19 +0800

    Add files via upload

---
+++ 2021_08/2021_08_22.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	char iKey[32] = { 0 };
+	char Key[32]  = { 0 };
+	int iID = 0xabc1d3f;
+	sprintf_s(Key, "%x", iID * 8 + 123456);
+	printf("你的机器码是%x\n", iID);
+	printf("请输入注册码:");
+	gets_s(iKey);
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	system("pause");
+	return 0;
+}
