e8862fb1d6e59e8942085d0bb270bfd26c446095
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-22 22:55:19 +0800

    Update 2021_04_04.cpp

--- 2021_04/2021_04_04.cpp
+++ 2021_04/2021_04_04.cpp
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <math.h>
 #include <stdlib.h>
 
 int main()
 {
-	printf("this is a program!\n");
+	char iKey[32] = { 0 };
+	char Key[32] = { 0 };
+	int iID = 0xabc1d3f;
+	sprintf_s(Key, "%x", iID * 8 + 123456);
+	printf("你的机器码是%x\n", iID);
+	printf("请输入注册码:");
+	gets_s(iKey);
+	if (strcmp(Key, iKey) == 0)
+	{
+		MessageBoxA(0, "注册成功","", MB_OK);
+	}
+	else
+	{
+		MessageBoxA(0, "注册失败","", MB_OK);
+	}
 	system("pause");
 	return 0;
 }
