2af62d35a980e69feaa0b29ba547bc1a500d7d49
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-22 23:02:22 +0800

    Update 2021_05_01.cpp

--- 2021_05/2021_05_01.cpp
+++ 2021_05/2021_05_01.cpp
@@ -1,9 +1,80 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <math.h>
 
 int main()
 {
-	printf("You're not a loser.\n");
-	system("pause");
+	char iKey[32] = { 0 };
+	char Key[32]  = { 0 };
+	int iID = 0xabc1d3f;
+	sprintf_s(Key, "%x", iID * 8 + 123456);
+	printf("你的机器码是%x\n", iID);
+	printf("请输入注册码:");
+	gets_s(iKey);
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
+	if (strcmp(Key, iKey) == 0)
+	{
+		printf("注册成功");
+	}
+	else
+	{
+		exit(0);
+	}
 	return 0;
 }
