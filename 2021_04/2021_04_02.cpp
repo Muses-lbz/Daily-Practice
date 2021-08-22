53d569dde4f70fdf2cc566a64e8cc7508dfbb0df
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-22 21:57:04 +0800

    Update 2021_04_02.cpp

--- 2021_04/2021_04_02.cpp
+++ 2021_04/2021_04_02.cpp
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main()
 {
-	printf("this is a program!\n");
+	char Key[32] = { 0 };
+	printf("请输入注册码:");
+	gets_s(Key);
+	if (strcmp(Key, "abc123456") == 0)
+	{
+		printf("注册成功\n\n");
+	}
+	else
+	{
+		printf("注册失败\n\n");
+	}
 	system("pause");
 	return 0;
 }
