18547c2ac48b3467e74b4a5639ccedc9e97e7a2d
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-23 22:23:41 +0800

    Update 2021_04_05.cpp

--- 2021_04/2021_04_05.cpp
+++ 2021_04/2021_04_05.cpp
@@ -1,9 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
 int main()
 {
-	printf("this is a program!\n");
+	int a, count, i;
+	count = 0;
+	scanf_s("%d", &a);
+	for (i = 2; i <= a - 1; i++)
+	{
+		if (a % i == 0)
+			count++;
+	}
+	if (count == 0)
+	{
+		printf("质数\n\n");
+	}
+	else
+	{
+		printf("合数\n\n");
+	}
 	system("pause");
 	return 0;
 }
