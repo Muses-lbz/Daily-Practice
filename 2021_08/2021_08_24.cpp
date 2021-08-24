7239eca6a6a5812b2fba8b7210b8c0c93fa26ede
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-08-24 22:20:22 +0800

    Add files via upload

---
+++ 2021_08/2021_08_24.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a, b, c;
+	scanf_s("%d%d", &a, &b);
+	c = a + b;
+	printf("%d+%d=%d\n", a, b, c);
+	c = a - b;
+	printf("%d-%d=%d\n", a, b, c);
+	c = a * b;
+	printf("%d*%d=%d\n", a, b, c);
+	c = a / b;
+	printf("%d/%d=%d\n", a, b, c);
+	system("pause");
+	return 0;
+}
