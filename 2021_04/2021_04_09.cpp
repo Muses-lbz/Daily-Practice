9f9ebcb251219da0fdda3dd96a9954798d03193c
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-04 22:47:59 +0800

    Update 2021_04_09.cpp

--- 2021_04/2021_04_09.cpp
+++ 2021_04/2021_04_09.cpp
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
 int main()
 {
-	printf("this is a program!\n");
+	char a;
+	a = getchar();
+	printf("你刚才输入的字符是：%c\n\n", a);
 	system("pause");
 	return 0;
 }
