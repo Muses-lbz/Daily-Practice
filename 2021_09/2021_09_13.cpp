b7b866d5115f9fa132daaa34527cfb28e5575978
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-13 23:39:08 +0800

    Add files via upload

---
+++ 2021_09/2021_09_13.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Student						//声明一个类，类名为Student
+{
+private:							//以下为类的私有内容
+	int num;						//私有变量num
+	int score;						//私有变量score
+public:							    //以下为类中公用部分
+	void setdata()					//定义公用函数setdata
+	{
+		cin >> num;					//输入num的值
+		cin >> score;				//输入score的值
+	}
+	void display()					//定义公用函数diaplay
+	{
+		cout << "num=" << num << endl;		//输出num的值
+		cout << "score=" << score << endl;	//输出score的值
+	};
+};									//类的声明结束
+
+Student stud1, stud2;				//定义stud1和stud2为Student类的变量，称为对象
+
+int main()							//主函数首部
+{
+	stud1.setdata();				//调用对象stud1的setdata函数
+	stud1.display();
+	stud2.setdata();
+	stud2.display();
+	system("pause");
+	return 0;
+}
+//运行结果：
+//01 90
+//num = 1
+//score = 90
+//02 100
+//num = 2
+//score = 100
