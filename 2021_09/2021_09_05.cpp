e7413d76209d86ce84e56d5b821fe49e789ee006
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-14 23:11:53 +0800

    Add files via upload

---
+++ 2021_09/2021_09_05.cpp
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int myAtoi(char* str)
+{
+	int i = 0;
+	int sign = 0;
+	while (str[i] && str[i] == ' ')
+		i++;
+	if (str[i] == NULL)
+		return 0;
+	if (str[i] == '-')
+	{
+		sign = 1;
+		i++;
+	}
+	else if (str[i] == '+')
+	{
+		sign = 0;
+		i++;
+	}
+	else if (str[i] < '0')
+	{
+		return 0;
+	}
+	else if (str[i] > '9')
+	{
+		return 0;
+	}
+	long long int r = 0;
+	while (str[i])
+	{
+		if (str[i] < '0')
+			break;
+		else if (str[i] > '9')
+			break;
+		else
+			r = r * 10 + str[i++] - '0';
+		if (r > INT_MAX)
+			break;
+	}
+	r = sign ? -r : r;
+	if (r < INT_MIN)
+		return INT_MIN;
+	if (r > INT_MAX)
+		return INT_MAX;
+	return (int)r;
+
+}
+
+int main()
+{
+	char* str;
+
+	system("pause");
+	return 0;
+}
