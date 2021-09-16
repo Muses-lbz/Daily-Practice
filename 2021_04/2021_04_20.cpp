ac4b13fe87f74acf976f2cefde6792b6413c7be8
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-16 19:11:26 +0800

    Update 2021_04_20.cpp

--- 2021_04/2021_04_20.cpp
+++ 2021_04/2021_04_20.cpp
@@ -1,33 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int X()
-{
-	printf("蔺");
-	return 0;
-}
-
-int Y()
-{
-	printf("兵");
-	return 0;
-}
-
-int Z()
-{
-	printf("姿");
-	return 0;
-}
-
 int main()
 {
-	X();
-	Y();
-	Z();
-	printf("\n");
-	Y();
-	Z();
-	printf("\n");
+	int k, a, b, i, count1, count2;
+	for (k = 4; k <= 100; k = k + 2)
+	{
+		for (a = 2; a <= k / 2; a++)
+		{
+			//判断a是否为质数
+			count1 = 0;
+			for (i = 2; i <= a - 1; i++)
+			{
+				if (a % i == 0)
+				{
+					count1++;
+					break;
+				}
+			}
+			if (count1 == 0) //如果a为质数
+			{
+				b = k - a;
+				//判断b是否为质数 
+				count2 = 0;
+				for (i = 2; i <= b - 1; i++)
+				{
+					if (b % i == 0)
+					{
+						count2++;
+						break;
+					}
+				}
+				if (count2 == 0) //如果b也是质数
+				{
+					printf("%d=%d+%d\n", k, a, b); break; //打印这个解并跳出循环
+				}
+			}
+		}
+	}
 	system("pause");
 	return 0;
 }
