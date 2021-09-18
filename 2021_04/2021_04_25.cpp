dc2b6aeb09609454da6b13a11c46eadfb63db134
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-18 20:57:01 +0800

    Update 2021_04_25.cpp

--- 2021_04/2021_04_25.cpp
+++ 2021_04/2021_04_25.cpp
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int X()
-{
-	printf("蔺");
-	return 0;
-}
-
-int Y()
-{
-	printf("兵");
-	return 0;
-}
-
-int Z()
-{
-	printf("姿");
-	return 0;
-}
-
 int main()
 {
-	X();
-	Y();
-	Z();
-	printf("\n");
-	Y();
-	Z();
-	printf("\n");
+	int a[6][6];
+	int m, n;
+	int i, j;
+	int max = 0;
+	int indexx = 0, indexy = 0;
+	scanf_s("%d %d", &m, &n);
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			scanf_s("%d", &a[i][j]);
+			if (i == 0 && j == 0)
+			{
+				max = a[i][j];
+				indexx = 0;
+				indexy = 0;
+			}
+			else
+			{
+				if (a[i][j] > max)
+				{
+					max = a[i][j];
+					indexx = i;
+					indexy = j;
+				}
+			}
+		}
+	}
+	printf("%d,%d,%d\n", max, indexx, indexy);
+	//PS:输出最大值及其所在位置的行列下标值
 	system("pause");
 	return 0;
 }
