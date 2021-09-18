c535d35681e9a1d3ec22ace8f1794b82f4df2bf7
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-18 20:41:43 +0800

    Update 2021_04_24.cpp

--- 2021_04/2021_04_24.cpp
+++ 2021_04/2021_04_24.cpp
@@ -1,33 +1,37 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <stdlib.h>
-
-int X()
-{
-	printf("蔺");
-	return 0;
-}
-
-int Y()
-{
-	printf("兵");
-	return 0;
-}
-
-int Z()
-{
-	printf("姿");
-	return 0;
-}
+using namespace std;
 
 int main()
 {
-	X();
-	Y();
-	Z();
-	printf("\n");
-	Y();
-	Z();
-	printf("\n");
+	int result[12] = { 0 };
+	int gold;
+	while (cin >> gold)
+	{
+		if (gold < 0)
+		{
+			break;
+		}
+		int code = gold / 10;
+		if (code < 4)
+		{
+			result[3] ++;
+		}
+		else if (code == 10)
+		{
+			result[9] ++;
+		}
+		else
+		{
+			result[code] ++;
+		}
+	}
+	string word[] = { "0~39", "40~49", "50~59","60~69","70~79", "80~89","90~100" };
+	for (int i = 0; i < 7; i++)
+	{
+		cout << word[i] << " " << result[i + 3] << endl;
+	}
 	system("pause");
 	return 0;
 }
