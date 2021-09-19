2bdb4b6eac9bb065a2c9f93931d25cfb62888c58
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-19 18:05:13 +0800

    Update 2021_04_27.cpp

--- 2021_04/2021_04_27.cpp
+++ 2021_04/2021_04_27.cpp
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int X()
-{
-	printf("蔺");
-	return 0;
-}
-
-int Y()
-{
-	printf("兵");
-	return 0;
-}
-
-int Z()
-{
-	printf("姿");
-	return 0;
-}
-
 int main()
 {
-	X();
-	Y();
-	Z();
-	printf("\n");
-	Y();
-	Z();
-	printf("\n");
+	int a, b, c, d, e, f;
+	int i = 1;
+	for (a = 1; a < 7; a++)
+	{
+		for (b = 1; b < 7; b++)
+		{
+			for (c = 1; c < 7; c++)
+			{
+				for (d = 1; d < 7; d++)
+				{
+					for (e = 1; e < 7; e++)
+					{
+						for (f = 1; f < 7; f++)
+						{
+							if (a != b && a != c && a != d && a != e && a != f && b != c && b != d && b != e && b != f && c != d && c != e && c != f && d != e && d != f && e != f)
+							{
+								if (a + b + d == a + c + f && a + c + f == d + e + f)
+								{
+									printf("第%2d种情况：%d %d %d %d %d %d\n", i, a, b, c, d, e, f);
+									i++;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 	system("pause");
 	return 0;
 }
