48a9fd498f8adcb0bdf2d0cd74d77a720bbf32c4
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-19 23:50:17 +0800

    Update 2021_05_09.cpp

--- 2021_05/2021_05_09.cpp
+++ 2021_05/2021_05_09.cpp
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 int main()
 {
-	printf("You're not a loser.\n");
+	for (int a = 2; a >= 0; a--)
+	{
+		for (int b = 60; b >= 0; b--)
+		{
+			system("cls");
+			if (a == 2)
+			{
+				printf("%d:00", a);
+				Sleep(500);
+				a = a - 1;
+			}
+			else if (a == 0 && b == 60)
+			{
+				continue;
+			}
+			else if ((b <= 9 && b >= 0) && (a == 1 || a == 0))
+			{
+				printf("%d:0%d", a, b);
+				Sleep(500);
+			}
+			else
+			{
+				printf("%d:%d", a, b);
+				Sleep(500);
+			}
+		}
+	}
+	system("cls");
+	printf("点火！\n");
 	system("pause");
 	return 0;
 }
