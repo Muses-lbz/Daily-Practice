4fc63a662e050dc3575513294c4f41a3d21f1d72
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 10:06:11 +0800

    Update 2021_07_02.cpp

--- 2021_07/2021_07_02.cpp
+++ 2021_07/2021_07_02.cpp
@@ -3,7 +3,18 @@
 
 int main()
 {
-	printf("HeHe!\n");
+
+	int a[10], i, t; 
+	for (i = 0; i <= 9; i++)
+		a[i] = 0; //初始化每个小房间为0
+	for (i = 1; i <= 5; i++)
+	{
+		scanf_s("%d", &t); //依次读入5个数
+		a[t]=1;         //把对应的小房间改为1
+	}
+	for (i = 0; i <= 9; i++)
+		if (a[i] == 0)     //输出没有出现过的数
+			printf("%d ", i);
 	system("pause");
 	return 0;
 }
