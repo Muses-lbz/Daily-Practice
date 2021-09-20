d66da90ba101f6bec3b94e7ecc4a6618becc3782
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 09:59:57 +0800

    Update 2021_06_30.cpp

--- 2021_06/2021_06_30.cpp
+++ 2021_06/2021_06_30.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
-	printf("You're not a loser.\n");
+	int h, a[10], i, sum; for (i = 0; i <= 9; i++)
+		scanf_s("%d", &a[i]);
+	scanf_s("%d", &h); sum = 0;
+	for (i = 0; i <= 9; i++)
+	{
+		if (a[i] <= h + 30)
+			sum++;
+	}
+	printf("%d\n", sum);
 	system("pause");
 	return 0;
 }
