baef0a2f534f398bdfff4906424a955257c09993
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 23:05:41 +0800

    Update 2021_07_09.cpp

--- 2021_07/2021_07_09.cpp
+++ 2021_07/2021_07_09.cpp
@@ -3,7 +3,29 @@
 
 int main()
 {
-	printf("HeHe!\n");
+
+	char a[101], b[101];
+	int x, y; 
+	gets_s(a);
+	scanf_s("%d", &x);
+	while (getchar() != '\n');
+	gets_s(b);
+	scanf_s("%d", &y); 
+	if (x > y)
+	{
+		printf("%s\n", a);
+	}
+	else
+	{
+		if (x < y)
+		{
+			printf("%s\n", b);
+		}
+		else
+		{
+			printf("%s和%s的分数相同\n", a, b);
+		}
+	}
 	system("pause");
 	return 0;
 }
