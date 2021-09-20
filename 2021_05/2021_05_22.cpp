a294aaebfec7019c40f89c7f4f6db02b21df18f6
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 08:46:14 +0800

    Update 2021_05_22.cpp

--- 2021_05/2021_05_22.cpp
+++ 2021_05/2021_05_22.cpp
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-	printf("You're not a loser.\n");
+	int a, b, sum = 6;
+	srand((unsigned)time(NULL));
+	a = rand() % 100;
+	while (1)
+	{
+		sum--;
+		scanf_s("%d", &b);
+		if (b > a)
+			printf("大了，请继续\n");
+		if (b < a)
+			printf("小了，请继续\n");
+		if (b == a)
+		{
+			printf("恭喜你答对了\n");
+			break;
+		}
+		if (sum == 0)
+		{
+			printf("LOSE!");
+			break;
+		}
+	}
 	system("pause");
 	return 0;
 }
