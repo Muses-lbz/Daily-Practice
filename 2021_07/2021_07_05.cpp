ee0c94bca22d6bcb0727b82aa1eb459dc7ac9572
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 10:27:00 +0800

    Update 2021_07_05.cpp

--- 2021_07/2021_07_05.cpp
+++ 2021_07/2021_07_05.cpp
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+	int a, b;
+	char c;
+	scanf_s("%d", &a);
+	scanf_s("%c", &c, 1);
+	scanf_s("%d", &b);
+	if (c == '+')
+		printf("%d\n", a + b);
+	if (c == '-')
+		printf("%d\n", a - b);
+	if (c == '＊')
+		printf("%d\n", a * b);
+	if (c == '/')
+		printf("%d\n", a / b);
+	system("pause");
+	return 0;
+}
