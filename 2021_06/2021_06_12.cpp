cf727ac837f8538f1a44a5dc77499436aae74492
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 09:51:25 +0800

    Update 2021_06_12.cpp

--- 2021_06/2021_06_12.cpp
+++ 2021_06/2021_06_12.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
-	printf("You're not a loser.\n");
+	int a[5], i;
+	for (i = 0; i <= 4; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	for (i = 4; i >= 0; i--)
+	{
+		printf("%d ", a[i]);
+	}
 	system("pause");
 	return 0;
 }
