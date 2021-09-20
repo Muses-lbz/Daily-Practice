e926ea83779121363aa379a0f2896fd74ec2453a
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 23:11:44 +0800

    Update 2021_07_10.cpp

--- 2021_07/2021_07_10.cpp
+++ 2021_07/2021_07_10.cpp
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-	printf("HeHe!\n");
+	char a[101] = { 0 }, t;
+	int len, i, j;
+	gets_s(a);
+	len = strlen(a);
+	for (i = 0; i <= len - 2; i++)
+	{
+		for (j = i + 1; j <= len - 1; j++)
+		{
+			if (a[i] > a[j])
+			{
+				t = a[i]; a[i] = a[j]; a[j] = t;
+			}
+		}
+	}
+	puts(a);
 	system("pause");
 	return 0;
 }
