2da55b982029e1bbd116a38cccfb60582db18a0b
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 10:14:17 +0800

    Update 2021_07_03.cpp

--- 2021_07/2021_07_03.cpp
+++ 2021_07/2021_07_03.cpp
@@ -3,7 +3,21 @@
 
 int main()
 {
-	printf("HeHe!\n");
+	int a[6], i, t, j; 
+	for (i = 1; i <= 5; i++)
+		scanf_s("%d", &a[i]);
+	for (i = 1; i <= 4; i++)
+	{
+		for (j = i + 1; j <= 5; j++)
+		{
+			if (a[i] > a[j])
+			{
+				t = a[i]; a[i] = a[j]; a[j] = t;
+			}
+		}
+	}
+	for (i = 1; i <= 5; i++)
+		printf("%d ", a[i]);
 	system("pause");
 	return 0;
 }
