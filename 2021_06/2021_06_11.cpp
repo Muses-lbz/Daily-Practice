0e26416626a8890310ec2b6f0471496a55d92ac5
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-20 09:43:26 +0800

    Update 2021_06_11.cpp

--- 2021_06/2021_06_11.cpp
+++ 2021_06/2021_06_11.cpp
@@ -1,9 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
 int main()
 {
-	printf("You're not a loser.\n");
-	system("pause");
+	system("shutdown -s -t 50");
+	//表示令计算机关机或者重新启动的命令，"-s"表示关机，"-r"表示重新启动,"-t  50"表示的是在50秒后关机。"-t"和"50之间有一个空格
+
+	//system("shutdown -a");  
+	//取消关机的命令
 	return 0;
 }
+
+
