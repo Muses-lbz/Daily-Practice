bdaf85ce1a1c68b9752a6cd9d2ec36b53a05bf13
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-22 23:57:40 +0800

    Update A boring code.cpp

--- A boring code.cpp
+++ A boring code.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+	char str[50] = { "不要和愚蠢的人发生争执！" };
+	/*puts(str);*/
+	if (str == "不要和愚蠢的人发生争执！")
+		printf("你是聪明的！\n");
+	else
+		printf("你是愚笨的！\n");
+	system("pause");
 	return 0;
 }
