31c7c086e73b11ed481f3281992c72e7272236f4
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-22 23:46:37 +0800

    Update 2021_07_20.cpp

--- 2021_07/2021_07_20.cpp
+++ 2021_07/2021_07_20.cpp
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
 
 int main()
 {
-	printf("不要和愚蠢的人发生争执！\n\n");
+	char str[50][50] = { "*蔺******","**兵*****","***姿****","****我***","*****想**","******你*" };
+	int love = 10;
+	for (int i = 0; i <= 5; i++)
+		puts(str[i]);
+	char ch;
+	while (love)
+	{
+		ch = _getch();
+		if (ch == 'y')
+		{
+			printf("汉语言文学\n");
+		}
+		else if (ch == 'n')
+		{
+			printf("失败\n");
+			love = 0;
+		}
+		else
+		{
+			printf("未知\n");
+			love = 0;
+		}
+	}
 	system("pause");
 	return 0;
 }
+
