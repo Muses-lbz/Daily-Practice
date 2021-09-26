1c18d59bdd21d5fdd1cee057b1fd3cb59fb6138a
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-26 10:26:06 +0800

    Update 2021_07_22.cpp

--- 2021_07/2021_07_22.cpp
+++ 2021_07/2021_07_22.cpp
@@ -1,9 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+string a[105];
+int b[105], c[105];
+
+int qiujishu(string x)
+{
+	int jishu = 0;
+	for (int i = 0; x.c_str()[i]; i++)
+	{
+		if ((x.c_str()[i] - '0') % 2 == 1) jishu++;
+	}
+	return jishu;
+}
+int qiuoushu(string x)
+{
+	int oushu = 0;
+	for (int i = 0; x.c_str()[i]; i++)
+	{
+		if ((x.c_str()[i] - '0') % 2 == 0) oushu++;
+	}
+	return oushu;
+}
 
 int main()
 {
-	printf("不要和愚蠢的人发生争执！\n\n");
-	system("pause");
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+		b[i] = qiujishu(a[i]);
+		c[i] = qiuoushu(a[i]);
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		cout << b[i] << " " << c[i] << endl;
+	}
 	return 0;
 }
