cbcd44d2994db46ebfa1a4d78f820a090cc56284
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-26 10:32:47 +0800

    Update 2021_07_23.cpp

--- 2021_07/2021_07_23.cpp
+++ 2021_07/2021_07_23.cpp
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main() 
 {
-	printf("不要和愚蠢的人发生争执！\n\n");
-	system("pause");
-	return 0;
+    int i, max = 0, min = 0;
+    double sum = 0, ave = 0;
+    int a[10];
+    for (i = 0; i < 10; i++)
+    {
+        scanf_s("%d", &a[i]);
+        if (i >= 0)
+            min = a[i];
+        sum += a[i];
+        if (max >= a[i])
+            max = a[i];
+        if (min > a[i])
+            min = a[i];
+    }
+    ave = sum / 10;
+    for (i = 0; i < 5; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+    for (i = 5; i < 10; i++)
+        printf("%d ", a[i]);
+    printf("\n平均成绩%f,最高分%d,最低分%d\n", ave, max, min);
+    system("pause");
+    return 0;
 }
+
