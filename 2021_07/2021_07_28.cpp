c1e1fd2ffc0a6e336847eaa43e1b669040e84cbb
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-27 17:00:43 +0800

    Update 2021_07_28.cpp

--- 2021_07/2021_07_28.cpp
+++ 2021_07/2021_07_28.cpp
@@ -1,9 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
+void butler(void); /* ANSI/ISO C 函数原型 */
 
-int main()
+int  main(void)
 {
-	printf("不要和愚蠢的人发生争执！\n\n");
+	printf("I  will  summon  the  butler function.\n");
+	butler();
+	printf("Yes.  Bring  me  some  tea  and  writable DVDs.\n");
 	system("pause");
-	return 0;
+	return  0;
 }
+void butler(void)
+/*函数定义开始 */
+{
+	printf("You  rang, sir ? \n");
+}
+
+
