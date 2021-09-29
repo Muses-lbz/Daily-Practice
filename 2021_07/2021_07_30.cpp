70bdd2a8cf80d576a82466414838dc82a35cf6bf
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-29 08:46:50 +0800

    Update 2021_07_30.cpp

--- 2021_07/2021_07_30.cpp
+++ 2021_07/2021_07_30.cpp
@@ -1,9 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-	printf("不要和愚蠢的人发生争执！\n\n");
-	system("pause");
+	int n, k, count = 1;
+	long long c;
+	cin >> n;
+	while (n--)
+	{
+		cin >> k >> c;
+		int num = k;
+		long long sum = k;
+		for (long long i = 2; i <= c; i = sum / k + 1)
+		{
+			int t = num;
+			num += (double)ceil((k * i - sum) * 1.0 / i);
+			sum += i * ((double)num - t);
+		}
+		cout << "Case #" << count++ << ": " << num << endl;
+	}
 	return 0;
 }
