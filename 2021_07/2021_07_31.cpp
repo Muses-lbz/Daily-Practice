0c329faec3ef7c6e1c7b84bf2400187065229a7e
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-29 08:48:45 +0800

    Update 2021_07_31.cpp

--- 2021_07/2021_07_31.cpp
+++ 2021_07/2021_07_31.cpp
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double fun(int n)
+{
+    double sum = 1.0;
+    int i;
+    for (i = 1; i < n; i++)
+        sum *= i;
+    return sum;
+}
 int main()
 {
-	printf("不要和愚蠢的人发生争执！\n\n");
-	system("pause");
-	return 0;
+    int i, mark = 1;
+    double sum = 0, item = 1;
+    for (i = 1; i <= 10; i++)
+    {
+        item = mark * fun(i);
+        sum += item;
+        mark = -mark;
+    }
+    printf("1!-2!+3!-4!+5!-6!+7!-8!+9!-10!=%.0lf\n", sum);
+    system("pause");
+    return 0;
 }
