0ee792e772097eb0a305ff6a2c438b45e54866d0
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-04 17:09:27 +0800

    Add files via upload

---
+++ 2021_09/2021_09_28.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+void funShift(int m, char* p, int n)
+{
+    char c;
+    for (int j = 0; j < m; j++)
+    {
+        c = p[n - 1];
+        for (int i = n - 1; i > 0; i--)
+        {
+            p[i] = p[i - 1];
+        }
+        p[0] = c;
+    }
+}
+
+int main()
+{
+    int i, m, n;
+    cin >> m >> n;
+    char* p = new char[n + 1];
+    p[n] = 0;
+    for (i = 0; i < n; ++i)
+        cin >> p[i];
+    funShift(m, p, n);
+    for (i = 0; i < n; ++i)
+        cout << p[i] << ' ';
+    cout << endl;
+    delete[] p;
+    return 0;
+}
