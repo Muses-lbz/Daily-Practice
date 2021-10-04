d2381a19cea368dc294180c9317c198c3878a592
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-04 16:27:18 +0800

    Update 2021_08_03.cpp

--- 2021_08/2021_08_03.cpp
+++ 2021_08/2021_08_03.cpp
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+
+int oneNum(int x)
+{
+    int cnt = 0;
+    while (x)
+    {
+        cnt++;
+        x = x & (x - 1);
+    }
+    return cnt;
+}
+
+int cmp(const void* a, const void* b)
+{
+    int al, bl;
+    int ret;
+    al = *(int*)a;
+    bl = *(int*)b;
+    ret = oneNum(al) - oneNum(bl);
+    return  ret ? ret : al - bl;
+}
 
 int main()
 {
-	printf("Everything we do with our abilities is our responsibility.\n力之所为，一切皆责。\n\n");
-	system("pause");
-	return 0;
+    int s[] = { 1,2,3,5,6,7,8 }, i;
+    int len = sizeof(s) / sizeof(*s);
+    qsort(s, len, 4, cmp);
+    for (i = 0; i < len; i++)
+    {
+        printf("%d\n", s[i]);
+    }
+    system("pause");
+    return 0;
 }
