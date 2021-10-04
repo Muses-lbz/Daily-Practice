9f8ba11034d4c871028a3b1b008ad48039e2a376
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-04 17:50:03 +0800

    Add files via upload

---
+++ 2021_10/2021_10_01.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+	int x, y, sum;
+	srand((unsigned int)time(NULL));
+	x = rand() % 10;
+	y = rand() % 35;
+	sum = x + y;
+	printf("sun=%d", sum);
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
