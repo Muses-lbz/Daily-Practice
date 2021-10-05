fd721732e59c9141fc659e904f56324362a13a09
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-05 22:04:40 +0800

    Add files via upload

---
+++ 2021_10/2021_10_03.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h> 
+#include<string.h>
+#include<stdlib.h> 
+
+void func(char* p)
+{
+	strcpy(p, "hello world");
+}
+
+void test01()
+{
+	//在test01中分配了内存，分配在栈上
+	char buf[1024] = { 0 };
+	func(buf);
+	printf("%s\n", buf);
+}
+
+void printString(char* str)
+{
+	printf("%s\n", str);
+}
+
+void test02()
+{
+	char* p = (char*)malloc(sizeof(char) * 64);
+	if (p == 0)
+	{
+		printf("error!");
+	}
+	else
+	{
+		memset(p, 0, 64);//将内存块清零
+		strcpy(p, "hello world");
+		printString(p);
+	}
+
+}
+
+int main()
+{
+	test02();
+	system("pause");
+	return 0;
+}
+
