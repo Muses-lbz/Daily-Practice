940a3ec2a18572d21f01a4679d45503d3c29ae09
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-08 11:04:28 +0800

    Add files via upload

---
+++ 2021_10/2021_10_07.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+//CSDN的垃圾代码 还是无法成功运行
+
+typedef struct node
+{
+    char x;
+    node* next;
+} LList;
+
+int main()
+{
+    LList* header = NULL;
+    node* p;
+    int n;
+    scanf_s("%d", &n);
+    fflush(stdin);
+    for (int i = 0; i < n; i++)
+    {
+        if (!header)
+        {
+            header = (node*)malloc(sizeof(node));
+            p = header;
+            scanf_s("%c", &(header->x));
+            header->next = NULL;
+        }
+        else
+        {
+            p->next = (node*)malloc(sizeof(node));
+            p = p->next;
+            scanf_s("%c", &(p->x));
+            p->next = NULL;
+        }
+    }
+    p = header;
+    int i = 0;
+    while (p)
+    {
+        if (i++ % 2 == 0)
+            printf("%c", p->x);
+        p = p->next;
+    }
+    return 0;
+}
