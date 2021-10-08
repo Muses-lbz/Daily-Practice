5b531328067b34fc1fe20d875875fcaada8cd7f0
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-08 11:03:38 +0800

    Update 2021_08_31.cpp

--- 2021_08/2021_08_31.cpp
+++ 2021_08/2021_08_31.cpp
@@ -1,16 +1,44 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <stdlib.h>
+using namespace std;
 
-int main()
+int fun(string str);
+int main(void) 
 {
-	int a;
-	a = 1;
-	while (a <= 100)
-	{
-		if (a % 3 == 0 && a % 5 != 0) printf("%d ", a);
-		a = a + 1;
-	}
-	printf("\n\n");
-	system("pause");
-	return 0;
+    string str = "abc123 def45gh";
+    cout << fun(str);
+    return 0;
+}
+
+int fun(string str) 
+{
+    int index = -1;
+    int score = 0;
+    for (unsigned int i = 0; i < str.length(); i++)
+    {
+        if ((str[i] >= 48 && str[i] <= 57) && (i + 1 < str.length()) && (str[i + 1] >= 48 && str[i + 1] <= 57))
+        {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) 
+    {
+        return score;
+    }
+    score = str[index] - '0';
+    for (unsigned int i = index + 1; i < str.length(); i++)
+    {
+        if (str[i] >= 48 && str[i] <= 57) 
+        {
+            score *= 10;
+            score += str[i] - '0';
+        }
+        else {
+            break;
+        }
+    }
+    printf("%d\n\n", score);
+    system("pause");
 }
