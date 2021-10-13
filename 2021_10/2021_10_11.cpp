e8284271a84fe8c8c293ae219357d0e34bec3ccc
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-13 14:37:23 +0800

    Add files via upload

---
+++ 2021_10/2021_10_11.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define N 100
+//将一维数组中的元素循环左移 k 个位置 输入描述 第 1 行是一维数组元素的个数 n(数组大小) 
+//第 2 行是一个整数 k, 表示移动的位置 下面 n 行为数组的元素个数 输出描述 输出 n 行，表示移动后的数字
+int main()
+{
+	int k, a[N], b[N] = { 0 }, n, i;
+	scanf_s("%d", &n);
+	scanf_s("%d", &k);
+	for (i = 0; i < n; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	for (i = 0; i < k % n; i++)
+	{
+		b[i] = a[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - k % n)
+		{
+			a[i] = a[i + k % n];
+		}
+		else
+		{
+			a[i] = b[i - n + k % n];
+		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	system("pause");
+	return 0;
+}
