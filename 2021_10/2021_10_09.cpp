86732b6964356940c5e6d37b4c2948921d975667
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-13 14:36:49 +0800

    Add files via upload

---
+++ 2021_10/2021_10_09.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+	int* p = (int*)malloc(40);
+	//开辟内存空间
+	if (p == NULL)//开辟失败
+	{
+		return -1;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		*(p + i) = i;
+	}
+	free(p);
+	//释放内存空间
+	return 0;
+}
