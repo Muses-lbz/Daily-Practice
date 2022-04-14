cf5f248f7bb62cd15ec2d37d542c22f62ebb7d7e
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2022-04-14 23:59:00 +0800

    Add files via upload

---
+++ 2022_04/2022_04_12.cpp
@@ -0,0 +1,55 @@
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	int s[3];
+//	for (int i = 0; i < 3; i++)
+//		scanf_s("%d", &s[i]);
+//	int max = 0;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		if (max < s[i])
+//			max = s[i];
+//	}
+//	printf("%d\n",max);
+//	system("pause");
+//	return 0;
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char c;
+	int cnt = 0, count = 0;
+	do {
+		scanf_s("%c", &c, 1);
+		if (cnt == 0 && c == ' ')
+			continue;
+		if (cnt == 0 && c == '.')
+			break;
+		if (c == ' ')
+		{
+			if (count == 0)
+				printf("%d", cnt);
+			else
+				printf(" %d", cnt);
+			cnt = 0;
+			count++;
+			continue;
+		}
+		if (c == '.')
+		{
+			if (count == 0)
+				printf("%d", cnt);
+			else
+				printf(" %d", cnt);
+			break;
+		}
+		cnt++;
+	} while (c != '.');
+	return 0;
+}
