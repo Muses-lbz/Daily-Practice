a679eda8bd64c404b06d9d3d18f605777fa9bccd
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2023-07-05 22:58:49 +0800

    Add files via upload

---
+++ 2023_07/2023_07_05.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+	int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k)
+	{
+		// 如果 k 小于等于 numOnes，那么最大和的物品数量就是 k
+		if (k <= numOnes)
+		{
+			return k;
+		}
+		// 如果 k 大于 numOnes 且小于等于 (numOnes + numZeros)，那么最大和的物品数量就是 numOnes
+		else if (k > numOnes && k <= (numOnes + numZeros))
+		{
+			return numOnes;
+		}
+		// 否则，最大和的物品数量为 2 * numOnes - k + numZeros
+		else
+		{
+			return 2 * numOnes - k + numZeros;
+		}
+	}
+};
+
+int main()
+{
+	Solution solution;
+	int result = solution.kItemsWithMaximumSum(3, 2, 0, 2);
+	cout << result << endl;
+	system("pause");
+	return 0;
+}
