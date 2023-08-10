278f75ac9e3dedaa42a7b7f14f5add50324e0c40
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2023-08-10 16:30:15 +0800

    Add files via upload

---
+++ 2023_08/2023_08_07.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;           // 指向字符串开头的指针
+        int right = s.size() - 1; // 指向字符串末尾的指针
+
+        while (left < right) {
+            // 交换两个指针指向的字符
+            swap(s[left], s[right]);
+            // 移动指针
+            left++;
+            right--;
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<char> s = { 'h', 'e', 'l', 'l', 'o' };
+
+    solution.reverseString(s);
+
+    cout << "Reversed string: ";
+    for (char c : s) {
+        cout << c;
+    }
+    cout << endl;
+
+    return 0;
+}
