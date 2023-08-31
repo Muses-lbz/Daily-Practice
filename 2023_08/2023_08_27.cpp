9812d3d563e03ed654c70a44a9a8dfc6ceb47026
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2023-08-31 20:13:19 +0800

    Add files via upload

---
+++ 2023_08/2023_08_27.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) {
+            return {};
+        }
+
+        // 首先根据区间的起始位置进行排序
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> merged;
+        merged.push_back(intervals[0]);
+
+        for (const auto& interval : intervals) {
+            if (interval[0] <= merged.back()[1]) {
+                // 如果当前区间的起始位置小于等于前一个区间的结束位置，发生重叠
+                merged.back()[1] = max(merged.back()[1], interval[1]);
+            }
+            else {
+                // 没有重叠，直接添加新的区间
+                merged.push_back(interval);
+            }
+        }
+
+        return merged;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> intervals = { {1,3},{2,6},{8,10},{15,18} };
+    vector<vector<int>> result = s.merge(intervals);
+
+    for (const auto& interval : result) {
+        cout << "[" << interval[0] << "," << interval[1] << "] ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
---
+++ 2023_08/2023_08_31.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minTrioDegree(int n, vector<vector<int>>& edges) {
+        vector<vector<bool>> isConnected(n + 1, vector<bool>(n + 1, false));
+        vector<int> degrees(n + 1, 0);
+
+        for (const vector<int>& edge : edges) {
+            int u = edge[0], v = edge[1];
+            isConnected[u][v] = isConnected[v][u] = true;
+            degrees[u]++;
+            degrees[v]++;
+        }
+
+        int minDegree = INT_MAX;
+
+        for (int u = 1; u <= n; ++u) {
+            for (int v = u + 1; v <= n; ++v) {
+                if (isConnected[u][v]) {  // Only consider connected nodes
+                    for (int w = v + 1; w <= n; ++w) {
+                        if (isConnected[v][w] && isConnected[u][w]) {  // Form a trio
+                            int trioDegree = degrees[u] + degrees[v] + degrees[w] - 6;  // Subtract shared edges
+                            minDegree = min(minDegree, trioDegree);
+                        }
+                    }
+                }
+            }
+        }
+
+        return (minDegree == INT_MAX) ? -1 : minDegree;
+    }
+};
+
+int main() {
+    Solution solution;
+    int n = 6;
+    vector<vector<int>> edges = { {1, 2}, {1, 3}, {3, 2}, {4, 1}, {5, 2}, {3, 6} };
+    int result = solution.minTrioDegree(n, edges);
+    cout << "Minimum degree of connective triplets: " << result << endl;
+    return 0;
+}
