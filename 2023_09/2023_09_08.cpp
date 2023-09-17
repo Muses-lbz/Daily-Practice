deab48080e8e8cfc6c16c95b5a9d9453a812944f
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2023-09-17 22:28:27 +0800

    Add files via upload

---
+++ 2023_09/2023_09_08.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        // 计算实际到站时间
+        int actualArrivalTime = arrivalTime + delayedTime;
+
+        // 如果实际到站时间超过 24 小时，减去 24 小时
+        if (actualArrivalTime >= 24) {
+            actualArrivalTime -= 24;
+        }
+
+        return actualArrivalTime;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    int arrival = 12;
+    int delay = 12;
+    int result = solution.findDelayedArrivalTime(arrival,delay);
+    cout << "actualArrivalTime: " << result << endl;
+    return 0;
+}
