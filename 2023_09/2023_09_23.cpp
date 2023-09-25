38baa137130386b8c55d884499792e0909cbc772
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2023-09-25 16:07:53 +0800

    Add files via upload

---
+++ 2023_09/2023_09_23.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class LockingTree {
+public:
+    LockingTree(vector<int>& parent) {
+        int n = parent.size();
+        this->parent = parent;
+        this->lockNodeUser = vector<int>(n, -1);
+        this->children = vector<vector<int>>(n);
+        for (int i = 0; i < n; i++) {
+            int p = parent[i];
+            if (p != -1) {
+                children[p].emplace_back(i);
+            }
+        }
+    }
+
+    bool lock(int num, int user) {
+        if (lockNodeUser[num] == -1) {
+            lockNodeUser[num] = user;
+            return true;
+        }
+        return false;
+    }
+
+    bool unlock(int num, int user) {
+        if (lockNodeUser[num] == user) {
+            lockNodeUser[num] = -1;
+            return true;
+        }
+        return false;
+    }
+
+    bool upgrade(int num, int user) {
+        bool res = lockNodeUser[num] == -1 \
+            && !hasLockedAncestor(num) \
+            && checkAndUnlockDescendant(num);
+        if (res) {
+            lockNodeUser[num] = user;
+        }
+        return res;
+    }
+
+    bool hasLockedAncestor(int num) {
+        num = parent[num];
+        while (num != -1) {
+            if (lockNodeUser[num] != -1) {
+                return true;
+            }
+            num = parent[num];
+        }
+        return false;
+    }
+
+    bool checkAndUnlockDescendant(int num) {
+        bool res = lockNodeUser[num] != -1;
+        lockNodeUser[num] = -1;
+        for (int child : children[num]) {
+            res |= checkAndUnlockDescendant(child);
+        }
+        return res;
+    }
+
+private:
+    vector<int> parent;
+    vector<int> lockNodeUser;
+    vector<vector<int>> children;
+};
+
+int main() {
+    vector<int> parent = { -1, 0, 0, 1, 1, 2, 2 };
+    LockingTree lockingTree(parent);
+
+    cout << lockingTree.lock(2, 2) << endl;    // 返回 true
+    cout << lockingTree.unlock(2, 3) << endl;  // 返回 false
+    cout << lockingTree.unlock(2, 2) << endl;  // 返回 true
+    cout << lockingTree.lock(4, 5) << endl;    // 返回 true
+    cout << lockingTree.upgrade(0, 1) << endl; // 返回 true
+    cout << lockingTree.lock(0, 1) << endl;    // 返回 false
+
+    return 0;
+}
---
+++ 2023_09/2023_09_24.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class LRUCache {
+public:
+    struct ListNode {
+        int key;
+        int value;
+        ListNode* prev;
+        ListNode* next;
+        ListNode(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+
+    LRUCache(int capacity) {
+        capacity_ = capacity;
+        head_ = new ListNode(-1, -1);
+        tail_ = new ListNode(-1, -1);
+        head_->next = tail_;
+        tail_->prev = head_;
+    }
+
+    int get(int key) {
+        if (cache_.find(key) != cache_.end()) {
+            // 移动访问的节点到链表头部
+            ListNode* node = cache_[key];
+            moveToHead(node);
+            return node->value;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (cache_.find(key) != cache_.end()) {
+            // 更新已存在的节点的值，并移动到链表头部
+            ListNode* node = cache_[key];
+            node->value = value;
+            moveToHead(node);
+        }
+        else {
+            if (cache_.size() >= capacity_) {
+                // 移除链表尾部的节点（最久未使用的）
+                ListNode* tailNode = tail_->prev;
+                removeFromList(tailNode);
+                cache_.erase(tailNode->key);
+                delete tailNode;
+            }
+            // 创建新节点，并添加到链表头部
+            ListNode* newNode = new ListNode(key, value);
+            addToFront(newNode);
+            cache_[key] = newNode;
+        }
+    }
+
+    ~LRUCache() {
+        // 释放链表和哈希表的内存
+        ListNode* node = head_->next;
+        while (node != tail_) {
+            ListNode* temp = node;
+            node = node->next;
+            delete temp;
+        }
+        delete head_;
+        delete tail_;
+    }
+
+private:
+    int capacity_;
+    unordered_map<int, ListNode*> cache_;
+    ListNode* head_;
+    ListNode* tail_;
+
+    void moveToHead(ListNode* node) {
+        // 从链表中移除节点
+        removeFromList(node);
+        // 添加到链表头部
+        addToFront(node);
+    }
+
+    void removeFromList(ListNode* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void addToFront(ListNode* node) {
+        node->next = head_->next;
+        head_->next->prev = node;
+        head_->next = node;
+        node->prev = head_;
+    }
+};
+
+int main() {
+    LRUCache lRUCache(2);
+    lRUCache.put(1, 1);
+    lRUCache.put(2, 2);
+    cout << lRUCache.get(1) << endl; // 返回 1
+    lRUCache.put(3, 3); // 移除键 2
+    cout << lRUCache.get(2) << endl; // 返回 -1
+    lRUCache.put(4, 4); // 移除键 1
+    cout << lRUCache.get(1) << endl; // 返回 -1
+    cout << lRUCache.get(3) << endl; // 返回 3
+    cout << lRUCache.get(4) << endl; // 返回 4
+
+    return 0;
+}
