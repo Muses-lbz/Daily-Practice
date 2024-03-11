6afbb058162730a8d4af5b0c33c67cc99318b1bc
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2024-03-11 22:00:42 +0800

    Add files via upload

---
+++ 2024_03/2024_03_11.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        int n = (int)title.size();
+        int l = 0, r = 0;   // 单词左右边界（左闭右开）
+        while (r < n) {
+            while (r < n && title[r] != ' ') {
+                ++r;
+            }
+            // 对于每个单词按要求处理
+            if (r - l > 2) {
+                title[l] = toupper(title[l]); // 将首字母大写
+            }
+            ++l; // 移动到单词中的下一个字母
+            while (l < r) {
+                title[l] = tolower(title[l]); // 将其他字母变为小写
+                ++l; // 移动到单词中的下一个字母
+            }
+            l = ++r; // 移动到下一个单词的左边界
+        }
+        return title;
+    }
+};
+
+int main() {
+    Solution sol;
+    string title = "First leTTeR of EACH Word";
+    cout << "Original title: " << title << endl;
+    string capitalizedTitle = sol.capitalizeTitle(title);
+    cout << "Capitalized title: " << capitalizedTitle << endl;
+
+    return 0;
+}
