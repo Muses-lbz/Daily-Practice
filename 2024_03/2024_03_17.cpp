fc39b260e7ff981cd1f7ebcf83ada885ffb3b394
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2024-03-19 21:32:08 +0800

    Add files via upload

---
+++ 2024_03/2024_03_17.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int funny;
+    int Printf()
+    {
+        cout << funny << endl;
+        cout << "Hello World!" << endl;
+        return 0;
+    }
+private:
+
+protected:
+
+};
+
+int main()
+{
+    Solution S;
+    S.funny = 1314;
+    S.Printf();
+
+    int x = 10;
+    auto lambda = [x](int y) { return x + y; };
+    int result = lambda(5);
+    cout << result << endl;
+
+    return 0;
+}
