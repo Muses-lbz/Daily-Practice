55697f0f45e805819d4f1f23b724ae13abdf7b4f
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2024-06-20 18:00:08 +0800

    Add files via upload

---
+++ 2024_06/2024_06_16.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if (a == b) {
+            return -1;
+        }
+        else {
+            return (int)max(a.length(), b.length());
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    string a, b;
+
+    // 从标准输入读取字符串a和b
+    //cout << "Enter the first string: ";
+    //cin >> a;
+    //cout << "Enter the second string: ";
+    //cin >> b;
+
+    a = { "aaa" };
+    b = { "bbb" };
+
+    // 计算并输出最长特殊序列的长度
+    int result = solution.findLUSlength(a, b);
+    cout << "The length of the longest uncommon subsequence is: " << result << endl;
+
+    return 0;
+}
