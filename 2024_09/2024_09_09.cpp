98f6190fb560e673fd004ad0c0ccedd0cd200c81
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2024-10-09 23:14:50 +0800

    Add files via upload

---
+++ 2024_09/2024_09_09.cpp
@@ -0,0 +1,19 @@
+﻿#include <windows.h>
+#include <iostream>
+
+int main() {
+    // 替换为你群聊窗口的标题
+    const char* windowTitle = "残虹大府后勤处";
+    HWND hwnd = FindWindowA(NULL, windowTitle);
+
+    if (hwnd != NULL) {
+        RECT rect;
+        GetWindowRect(hwnd, &rect);
+        std::cout << "窗口坐标: (" << rect.left << ", " << rect.top << ")" << std::endl;
+    }
+    else {
+        std::cerr << "未找到窗口，请检查窗口标题。" << std::endl;
+    }
+
+    return 0;
+}
