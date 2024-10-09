5c1830e9eab0136fd025dcf05b14e16982030b51
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2024-10-09 23:15:13 +0800

    Add files via upload

---
+++ 2024_09/2024_09_10.cpp
@@ -0,0 +1,39 @@
+﻿//#include <windows.h>
+//#include <iostream>
+//
+//void ListWindows() {
+//    HWND hwnd = GetTopWindow(NULL);
+//    while (hwnd) {
+//        char title[256];
+//        GetWindowTextA(hwnd, title, sizeof(title));
+//        if (IsWindowVisible(hwnd) && strlen(title) > 0) {
+//            std::cout << "窗口句柄: " << hwnd << " | 标题: " << title << std::endl;
+//        }
+//        hwnd = GetNextWindow(hwnd, GW_HWNDNEXT);
+//    }
+//}
+//
+//int main() {
+//    ListWindows();
+//    return 0;
+//}
+
+#include <windows.h>
+#include <iostream>
+
+void ListWindows() {
+    HWND hwnd = GetTopWindow(NULL);
+    while (hwnd) {
+        char title[256];
+        GetWindowTextA(hwnd, title, sizeof(title));
+        if (IsWindowVisible(hwnd) && strlen(title) > 0) {
+            std::cout << "窗口句柄: " << hwnd << " | 标题: " << title << std::endl;
+        }
+        hwnd = GetNextWindow(hwnd, GW_HWNDNEXT);
+    }
+}
+
+int main() {
+    ListWindows();
+    return 0;
+}
