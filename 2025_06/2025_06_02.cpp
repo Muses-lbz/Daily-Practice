6e689068f80703d14df14b4ebc29fb4f2d8df2ba
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2025-06-30 21:20:24 +0800

    Add files via upload

---
+++ 2025_06/2025_06_02.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+// 判断是否是质数
+bool isPrime(int n) {
+    if (n <= 1) return false; // 小于等于1不是质数
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0) return false;
+    return true;
+}
+
+int main() {
+    int num = 29;
+    cout << num << (isPrime(num) ? " 是质数" : " 不是质数") << endl;
+    return 0;
+}
