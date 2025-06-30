dba9ca91699c8e14f5b1360cd22ce0eed51d96b3
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2025-06-30 21:20:50 +0800

    Add files via upload

---
+++ 2025_06/2025_06_05.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+// 反转数组
+void reverseArray(vector<int>& arr) {
+    int left = 0, right = arr.size() - 1;
+    while (left < right) {
+        swap(arr[left++], arr[right--]);
+    }
+}
+
+int main() {
+    vector<int> nums = { 1, 2, 3, 4, 5 };
+    reverseArray(nums);
+    for (int n : nums) cout << n << " ";
+    return 0;
+}
